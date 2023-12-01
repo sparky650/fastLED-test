bc739707d76f2ab5d9bb2a0db927d083f89fc8f0
Keegan Morrow <kmorrow@roto.com>
2023-12-01 18:03:31 -0500

    add test code

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,26 @@
 #include <Arduino.h>
+#include <FastLED.h>
 
-// put function declarations here:
-int myFunction(int, int);
+constexpr auto LED_PIN = 4;
+constexpr auto NUM_LEDS = 16;
+constexpr auto COLOR_ORDER = EOrder::GRB;
+#define LED_TYPE WS2811_400
 
-void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+CRGB leds[NUM_LEDS];
+
+void setup()
+{
+  FastLED.addLeds<LED_TYPE, LED_PIN, COLOR_ORDER>(leds, NUM_LEDS).setCorrection(TypicalLEDStrip);
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-}
-
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+void loop()
+{
+  static uint8_t hue = 0;
+  for (auto i = 0; i < NUM_LEDS; i++)
+  {
+    leds[i] = CHSV(hue + i, 255, 255);
+  }
+  FastLED.show();
+  hue++;
+  delay(25);
 }
